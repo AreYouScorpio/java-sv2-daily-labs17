ff2c898765b90742684d2b44394f3eeba8ef4e8f
AreYouScorpio <akos.schweger@gmail.com>
2022-02-15 16:41:11 +0100

    week17 day01 gyakorolgatás

--- src/main/java/day01/Main.java
+++ src/main/java/day01/Main.java
@@ -1,3 +1,7 @@
+// tábla létrehozása HeidiSQL-ben először:
+// CREATE TABLE actors(id BIGINT AUTO_INCREMENT, actor_name VARCHAR(255), CONSTRAINT pk_actors PRIMARY KEY (id));
+
+
 package day01;
 
 import org.mariadb.jdbc.MariaDbDataSource;
