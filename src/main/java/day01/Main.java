e0a572807cac2a9afa10aa25c9ec6f8b557d13e2
AreYouScorpio <akos.schweger@gmail.com>
2022-02-15 16:40:26 +0100

    week17 day01 gyakorolgatás

---
+++ src/main/java/day01/Main.java
@@ -0,0 +1,32 @@
+package day01;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+        try {
+            dataSource.setUrl("jdbc:mariadb://localhost:3306/movies-actors?useUnicode=true");
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
+        }
+        catch (SQLException sqle) {
+            throw new IllegalStateException("Cannot reach database!", sqle);
+        }
+
+        try (Connection connection = dataSource.getConnection()){
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("insert into actors(actor_name) values('John Doe')");
+        }
+        catch (SQLException sqle){
+            throw new IllegalStateException("Cannot connect!", sqle);
+        }
+
+    }
+
+}
